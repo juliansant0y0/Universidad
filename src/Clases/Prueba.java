dbddc765bcfafdbe00db2939af245f23afdaf081
Julian Santiago Santoyo Diaz <jsdiaz@o365.unab.edu.co>
2022-09-19 16:26:15 -0500

    Se agrega el proyecto java

---
+++ src/Clases/Prueba.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Clases;
+
+/**
+ *
+ * @author jsdiaz
+ */
+public class Prueba {
+    public static void main(String[] args) {
+        
+    }
+}
